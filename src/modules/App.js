c426bab5f7a0e65531cfeec642cad97004f413d1
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-09 22:23:30 +0545

    toast provider added

--- src/modules/App.js
+++ src/modules/App.js
@@ -9,24 +9,27 @@ import CreateWallet from '../modules/wallet/create';
 import ResetWallet from '../modules/misc/reset';
 import RestoreFromMnemonic from '../modules/wallet/restoreMnemonic';
 
+import { ToastProvider } from 'react-toast-notifications';
 import { AppContextProvider } from '../contexts/AppContext';
 
 function App() {
 	return (
 		<>
 			<AppContextProvider>
-				<BrowserRouter>
-					<Switch>
-						<Route exact path="/setup" component={Setup} />
-						<Route exact path="/create" component={CreateWallet} />
-						<Route exact path="/unlock" component={UnlockWallet} />
-						<Route exact path="/google/restore" component={GoogleRestore} />
-						<Route exact path="/mnemonic/restore" component={RestoreFromMnemonic} />
-						<Route exact path="/reset" component={ResetWallet} />
-						<Route path="/" component={Home} />
-						<Route path="*" component={Home} />
-					</Switch>
-				</BrowserRouter>
+				<ToastProvider>
+					<BrowserRouter>
+						<Switch>
+							<Route exact path="/setup" component={Setup} />
+							<Route exact path="/create" component={CreateWallet} />
+							<Route exact path="/unlock" component={UnlockWallet} />
+							<Route exact path="/google/restore" component={GoogleRestore} />
+							<Route exact path="/mnemonic/restore" component={RestoreFromMnemonic} />
+							<Route exact path="/reset" component={ResetWallet} />
+							<Route path="/" component={Home} />
+							<Route path="*" component={Home} />
+						</Switch>
+					</BrowserRouter>
+				</ToastProvider>
 			</AppContextProvider>
 		</>
 	);
