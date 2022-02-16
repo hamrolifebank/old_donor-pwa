e2c72e641061045d68e53e2740d2ae5e541edfb9
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-16 18:12:12 +0545

    routes added

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,6 +2,8 @@ const server_url = 'http://localhost:3011';
 const base_url = `${server_url}/api/v1`;
 
 module.exports = {
+	SERVER_URL: server_url,
+	BASE_URL: base_url,
 	EVENTS: `${base_url}/events`,
 	USERS: `${base_url}/users`
 };
