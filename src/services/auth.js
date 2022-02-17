ed2538e1cb196e2a9c357b9eb51ceb9391662438
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-17 17:26:31 +0545

    user details and token saved in localstorage

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,12 +1,18 @@
 import API from '../constants/api';
 import axios from 'axios';
+import { saveUser, saveUserToken, saveUserRoles } from '../utils/sessionmanager';
 
 const Auth = {
 	login(payload) {
 		return new Promise((resolve, reject) => {
 			axios
 				.post(`${API.SERVER_URL}/login_process`, payload)
-				.then(res => resolve(res.data))
+				.then(res => {
+					saveUser(res.data.user);
+					saveUserToken(res.data.user.token);
+					saveUserRoles(res.data.user.roles);
+					resolve(res.data);
+				})
 				.catch(e => reject(e.response));
 		});
 	},
@@ -15,7 +21,12 @@ const Auth = {
 		return new Promise((resolve, reject) => {
 			axios
 				.post(`${API.BASE_URL}/users/auth/googleLogin`, payload)
-				.then(res => resolve(res.data))
+				.then(res => {
+					saveUser(res.data.user);
+					saveUserToken(res.data.token);
+					saveUserRoles(res.data.user.roles);
+					resolve(res.data);
+				})
 				.catch(e => reject(e.response));
 		});
 	}
